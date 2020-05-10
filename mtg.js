88cf07b59ad8f52b44e059c9da5623db7ebcb104
Aaron William Langham <awlangham@gmail.com>
2020-05-10 12:17:33 -0400

    Added mtg.js and button functionality

---
+++ mtg.js
@@ -0,0 +1,24 @@
+$(function() {
+  var player1Life = 20;
+  var player2Life = 20;
+
+  $('.player1 .minusButton').click(function() {
+    --player1Life;
+    $('#player1LifeText').text(player1Life);
+  });
+
+  $('.player1 .plusButton').click(function() {
+    ++player1Life;
+    $('#player1LifeText').text(player1Life);
+  });
+
+  $('.player2 .minusButton').click(function() {
+    --player2Life;
+    $('#player2LifeText').text(player2Life);
+  });
+
+  $('.player2 .plusButton').click(function() {
+    ++player2Life;
+    $('#player2LifeText').text(player2Life);
+  });
+});
